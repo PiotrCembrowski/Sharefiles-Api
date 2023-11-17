5c91a1935a1cad9ee5f9673f3fd40eddca353faa
Piotr Cembrowski <losttape@gmail.com>
2023-11-17 18:40:05 +0100

    API with AUTH

--- go.mod
+++ go.mod
@@ -3,3 +3,10 @@ module github.com/PiotrCembrowski/Sharefiles-Api
 go 1.21.3
 
 require github.com/gorilla/mux v1.8.0
+
+require github.com/lib/pq v1.10.9
+
+require (
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
+	golang.org/x/crypto v0.15.0 // indirect
+)
--- types.go
+++ types.go
@@ -1,17 +1,47 @@
 package main
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
 
-type Account struct {
-	ID        int 		`json:"id"`
-	FirstName string 	`json:"firstName"`
-	LastName  string 	`json:"lastName"`
+	"golang.org/x/crypto/bcrypt"
+)
+
+type loginRequest struct{
+	Number 		int64 `json:"number"`
+	Password 	string `json:"password"`
 }
 
-func NewAccount(firstName, lastName string) *Account {
-	return &Account{
-		ID: rand.Intn(10000),
-		FirstName: firstName,
-		LastName: lastName,
+type TransferRequest struct {
+	ToAccount int `json:"toAccount"`
+	Amount  int `json:"amount"`
+}
+
+type CreateAccountRequest struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+type Account struct {
+	ID        			int 		`json:"id"`
+	FirstName 			string 		`json:"firstName"`
+	LastName  			string 		`json:"lastName"`
+	CreatedAt 			time.Time 	`json:"createdAt"`
+	Number	  			int64		`json:"number"`
+	EncryptedPassword 	string 		`json:"-"`
+	Balance	  			int64		`json:"balance"`
+}
+
+func NewAccount(firstName, lastName, password string) (*Account, error) {
+	encpw, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
 	}
+	return &Account{
+		FirstName: 			firstName,
+		LastName: 			lastName,
+		Number:				int64(rand.Intn(1000000)), 
+		EncryptedPassword: 	string(encpw),
+		CreatedAt: 			time.Now().UTC(),
+	}, nil
 }
